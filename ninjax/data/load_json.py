73bd47bc21c33216aaa03166422fdb496424c552
computer <sk99@uw.edu>
2024-08-24 19:09:36 -0700

    Modified load_json file to accept multiple json inputs

--- ninjax/data/load_json.py
+++ ninjax/data/load_json.py
@@ -1,16 +1,27 @@
 import os
 from functools import lru_cache
 from typing import Any, Dict, Optional, Union
-
 import orjson
 
-root = os.path.join(os.path.dirname(os.path.realpath(__file__)), "static")
+def load_json(file: str) -> Dict[str, Any]:
+    root = os.path.join(os.path.dirname(os.path.realpath(__file__)), "static")
+    if(file == "gen9moves.json"):
+        with open(
+            os.path.join(root, file)
+        ) as f:
+            return orjson.loads(f.read())
+    else:
+        root = os.path.join(os.path.dirname(os.path.realpath(__file__)), "static")
+        with open(os.path.join(root, "pokedex.json")) as f:
+            dex = orjson.loads(f.read())
+        dex = {k: v for k, v in dex.items() if v["num"] > 0}
+    # remove megas and gmax, currently only targeting gen 9
+        dex = {k: v for k, v in dex.items() if ("forme" not in v) or ("forme" in v and v["forme"] not in ["Mega", "Gmax"])}
+        return dex
+        
+   
 
-with open(
-        os.path.join(root, "pokedex.json")
-) as f:
-    dex = orjson.loads(f.read())
-# remove CAMOmons
-dex = {k: v for k, v in dex.items() if v["num"] > 0}
-# remove megas and gmax, currently only targeting gen 9
-dex = {k: v for k, v in dex.items() if ("forme" not in v) or ("forme" in v and v["forme"] not in ["Mega", "Gmax"])}
+
+
+
+
