mostly fixing broadcasting issues

ninjax/pokemon.py
@@ -1,5 +1,6 @@
 from typing import Union, Tuple, Dict, Any
 from dataclasses import field
+import dataclass_array as dca
 
 import chex
 from flax import struct
@@ -13,7 +14,7 @@ from dataclass_array import DataclassArray, dataclass_array
 from dataclass_array.typing import FloatArray, IntArray, BoolArray
 from ninjax.utils import STAT_MULTIPLIER_LOOKUP
 
-@dataclass_array(broadcast=True)
+@dataclass_array(broadcast=True, cast_list=True)
 class Pokemon(DataclassArray):
     type_list: IntArray['*batch_size 2']
     moves: Move['*batch_size 4']
@@ -58,8 +59,10 @@ class Pokemon(DataclassArray):
 
     @property
     def max_hp(self):
-        return self.stat_table.stats[0]
+        # TODO: idk if this works long term but im doing this as a hack right now
+        return self.stat_table.stats[...,0]
 
+    # TODO: this method doesnt seem to work properly for stacked pokemon
     def is_type(self, t: Type):
         return jnp.any(self.type_list==t)
 
@@ -89,6 +92,7 @@ class Pokemon(DataclassArray):
     @property
     def current_hp(self):
         return self.stat_table.current_hp
+
 
 
 

ninjax/side.py
@@ -6,6 +6,7 @@ import chex
 import jax.lax
 from dataclass_array import DataclassArray
 from dataclass_array.typing import FloatArray, IntArray, BoolArray
+import dataclass_array as dca
 from flax import struct
 import jax.numpy as jnp
 
@@ -25,22 +26,24 @@ class VolatileStatus:
     def replace_row(self, idx: int, new_status: 'VolatileStatus'):
         return self
 
-
+@dca.dataclass_array(broadcast=True)
 class BattleState(DataclassArray):
-    team: Pokemon
+    team: Pokemon['*batch_size 6']
     # figure out how to represent no pokemon on field, maybe active_index=-1?
     # or make a flag variable?
-    active_index: IntArray['*batch_size 1'] = field(default_factory=jnp.int32([0]))
-    stealth_rocks: BoolArray['*batch_size 1'] = field(default_factory=jnp.int32([0]))
-    sticky_webs: BoolArray['*batch_size 1'] = field(default_factory=jnp.int32([0]))
-    spikes: IntArray['*batch_size 1'] = field(default_factory=jnp.int32([0]))
-    toxic_spikes: IntArray['*batch_size 1'] = field(default_factory=jnp.int32([0]))
-    reflect: IntArray['*batch_size 1'] = field(default_factory=jnp.int32([0]))
-    light_screen: IntArray['*batch_size 1'] = field(default_factory=jnp.int32([0]))
-    aurora_veil: IntArray['*batch_size 1'] = field(default_factory=jnp.int32([0]))
-    tailwind: IntArray['*batch_size 1'] = field(default_factory=jnp.int32([0]))
-    toxic_counter: IntArray['*batch_size 1'] = field(default_factory=jnp.int32([0]))
-    boosts: StatBoosts = field(default_factory=StatBoosts)
+    active_index: IntArray['*batch_size 1'] = field(default_factory=lambda: jnp.int32([0]))
+    stealth_rocks: BoolArray['*batch_size 1'] = field(default_factory=lambda: jnp.bool([0]))
+    sticky_webs: BoolArray['*batch_size 1'] = field(default_factory=lambda: jnp.bool([0]))
+    spikes: IntArray['*batch_size 1'] = field(default_factory=lambda: jnp.int32([0]))
+    toxic_spikes: IntArray['*batch_size 1'] = field(default_factory=lambda: jnp.int32([0]))
+    reflect: IntArray['*batch_size 1'] = field(default_factory=lambda: jnp.int32([0]))
+    light_screen: IntArray['*batch_size 1'] = field(default_factory=lambda: jnp.int32([0]))
+    aurora_veil: IntArray['*batch_size 1'] = field(default_factory=lambda: jnp.int32([0]))
+    tailwind: IntArray['*batch_size 1'] = field(default_factory=lambda: jnp.int32([0]))
+    toxic_counter: IntArray['*batch_size 1'] = field(default_factory=lambda: jnp.int32([0]))
+    boosts: StatBoosts = StatBoosts(
+        normal_boosts=jnp.zeros((2, 6) ,dtype='int32'),
+        acc_boosts=jnp.zeros((2,2), dtype='int32'))
     legal_action_mask: jax.Array = field(default_factory=lambda: jnp.ones((2, 15)))
     can_tera: jax.Array = field(default_factory=lambda: jnp.ones(2))
     # notably volatile status needs like wish, healing wish, and future sight things

ninjax/stats.py
@@ -1,6 +1,8 @@
 from typing import Union, Tuple, Dict, Any
 from collections import namedtuple
 from dataclasses import field
+import dataclass_array as dca
+
 
 import chex
 from flax import struct
@@ -9,11 +11,13 @@ import jax.numpy as jnp
 import numpy as np
 from dataclass_array import DataclassArray
 from dataclass_array.typing import FloatArray, IntArray
+from flax.core import broadcast
 
 from ninjax.enum_types import StatEnum
 from ninjax.utils import calculate_stats, STAT_MULTIPLIER_LOOKUP
 
 Array = jax.Array
+int32 = jnp.int32
 
 class Nature(DataclassArray):
     increased: IntArray['*batch_size 1'] = field(default_factory=lambda: jnp.array([1]))
@@ -26,13 +30,15 @@ class Nature(DataclassArray):
 
 
 class StatBoosts(DataclassArray):
-    normal_boosts: IntArray['*batch_size 6'] = field(default_factory=lambda: jnp.zeros(6))
-    acc_boosts: IntArray['*batch_size 2'] = field(default_factory=lambda: jnp.zeros(2))
+    normal_boosts: IntArray['*batch_size 6'] = field(default_factory=lambda: jnp.zeros(6, dtype=int32))
+    acc_boosts: IntArray['*batch_size 2'] = field(default_factory=lambda: jnp.zeros(2, dtype=int32))
 
 
     def __post_init__(self) -> None:
-        self.normal_boosts = jnp.clip(self.normal_boosts, -6, 6)
-        self.acc_boosts = jnp.clip(self.acc_boosts, -6, 6)
+        super().__post_init__()
+        # use __setattr__ manually instead of attribute assignment since the class is frozen
+        object.__setattr__(self, "normal_boosts", jnp.clip(self.normal_boosts, -6, 6))
+        object.__setattr__(self, "acc_boosts", jnp.clip(self.normal_boosts, -6, 6))
 
     def replace_row(self, idx: int, new_boosts: 'StatBoosts'):
         normal_boosts = self.normal_boosts.at[idx].set(new_boosts.normal_boosts)
@@ -44,16 +50,26 @@ class StatBoosts(DataclassArray):
         acc_boosts = self.acc_boosts + other.acc_boosts
         return StatBoosts(normal_boosts=normal_boosts, acc_boosts=acc_boosts)
 
+@dca.dataclass_array(cast_dtype=True, broadcast=True)
 class StatTable(DataclassArray):
-    level: IntArray['*batch_size 1'] = field(default_factory=lambda: jnp.array(100))
+    level: IntArray['*batch_size 1'] = field(default_factory=lambda: jnp.array([100]))
     nature: Nature = Nature()
-    base_stats: IntArray['*batch_size 6'] = field(default_factory=lambda: jnp.zeros(6))
-    ivs: IntArray['*batch_size 6'] = field(default_factory=lambda: 31*jnp.ones(6))
-    evs: IntArray['*batch_size 6'] = field(default_factory=lambda: 84*jnp.ones(6))
+    base_stats: IntArray['*batch_size 6'] = field(default_factory=lambda: 100 * jnp.ones(6, dtype=int32))
+    ivs: IntArray['*batch_size 6'] = field(default_factory=lambda: 31*jnp.ones(6, dtype=int32))
+    evs: IntArray['*batch_size 6'] = field(default_factory=lambda: 84*jnp.ones(6, dtype=int32))
+    # this seems to be the cleanest way to implement these since they need fancy initialization
+    stats: IntArray['*batch_size 6'] = field(init=False, default=None)
+    current_hp: IntArray['*batch_size 1'] = field(init=False, default=None)
 
     def __post_init__(self) -> None:
-        self.stats = calculate_stats(self.level, self.nature, self.base_stats, self.ivs, self.evs)
-        self.current_hp = self.stats[0]
+        # make sure to call the super function, you fool, you absolute buffoon
+        super().__post_init__()
+        # use __setattr__ manually instead of attribute assignment since the class is frozen
+        # is equivalent to the following code
+        # self.stats = calculate_stats(self.level, self.nature, self.base_stats, self.ivs, self.evs))
+        # self.current_hp = self.stats[0]
+        object.__setattr__(self, 'stats', calculate_stats(self.level, self.nature, self.base_stats, self.ivs, self.evs))
+        object.__setattr__(self, 'current_hp', self.stats[...,0])
 
     def row_update(self, idx: int, new_stats: 'StatTable'):
         level = self.level.at[idx].set(new_stats.level)

ninjax/utils.py
@@ -44,17 +44,14 @@ def calculate_effectiveness_multiplier(attacking_type, defending_types) -> Array
     return jnp.prod(TYPE_EFFECTIVENESS[attacking_type][defending_types])
 
 @jax.jit
-def calculate_stats(level: int, nature: "Nature", base_stats: Array, ivs: Array, evs: Array):
+def calculate_stats(level: Array, nature: "Nature", base_stats: Array, ivs: Array, evs: Array):
     # initial part of compute
-    stats: Array = jnp.floor_divide((2 * base_stats + evs + ivs + jnp.floor(evs)) * level, 100) + 5
+    stats: Array = jnp.floor_divide((2 * base_stats + jnp.floor_divide(evs, 4) + ivs) * level, 100) + 5 + jnp.array([1, 0, 0, 0, 0, 0]) * (level + 5)
 
     # factor in nature modifiers
     stats_tenth = jnp.floor_divide(stats, 10)
     stats = stats.at[nature.increased].add(stats_tenth[nature.increased])
     stats = stats.at[nature.decreased].add(-stats_tenth[nature.decreased])
-
-    # HP has an extra increase
-    stats = stats.at[0].add(level + 5)
 
     return stats
 