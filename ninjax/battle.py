fixed more broadcasting errors and misc dumb typos i made

ninjax/battle.py
@@ -15,7 +15,7 @@ import numpy as np
 from ninjax.side import BattleState, step_side_conditions, swap_out, take_damage_percent, take_damage_value
 from ninjax.enum_types import StatEnum, WeatherEnum, TerrainEnum, Status, TurnType
 from ninjax.move import Move, MoveType
-from ninjax.utils import base_damage_compute, calculate_effectiveness_multiplier, static_len_array_access
+from ninjax.utils import base_damage_compute, calculate_effectiveness_multiplier
 
 Weather = namedtuple("Weather", ["weather", "duration"])
 Terrain = namedtuple("Terrain", ["terrain", "duration"])
@@ -127,7 +127,7 @@ def switch_move_step(
     # TODO: ugggghhhhhh, run it back if not bad, return the correct stuff
 
 
-@jit
+
 def action_order(
     state: BattleState,
     actions: (int, int),
@@ -158,7 +158,7 @@ def decode_action(action: int) -> (bool, int, bool):
 def conditional_mult_round(damage, mult, cond):
     return jnp.floor(damage * mult ** cond + 1 / 2)
 
-@partial(jit, static_argnums=2)
+
 def step_move(
     key: chex.PRNGKey,
     state: BattleState,
@@ -231,7 +231,7 @@ def step_switch(
     # TODO: add an opponent switched field somewhere for stakeout + analytic
     return key, swap_out(state, player_index, index)
 
-@jit
+
 def step_action(
     key: chex.PRNGKey,
     state: BattleState,

ninjax/pokemon.py
@@ -28,7 +28,9 @@ class Pokemon(DataclassArray):
     status: IntArray['*batch_size 1'] = field(default_factory=lambda: jnp.int32([0]))
     #ability: IntArray['*batch_size'] = jnp.int32([0])
     #item: IntArray['*batch_size'] = jnp.int32([0])
-    stat_table: StatTable = field(default_factory=StatTable)
+    stat_table: StatTable = StatTable()
+    # this is a hack. we
+    current_hp: IntArray['*batch_size 1'] = stat_table.stats[...,0].reshape(1)
     # add stats conditions and volatile status conditions
 
     def replace_row(self, idx: int, new_pokemon: "Pokemon"):
@@ -44,11 +46,13 @@ class Pokemon(DataclassArray):
         gender = self.gender.at[idx].set(new_pokemon.gender)
         is_terastallized = self.is_terastallized.at[idx].set(new_pokemon.is_terastallized)
         status = self.status.at[idx].set(new_pokemon.status)
-        stat_table = status.at[idx].set(new_pokemon.stat_table)
-        return self.replace(
-            type=type, is_alive=is_alive, gender=gender, is_terastallized=is_terastallized, status=status,
-            stat_table=stat_table
+        stat_table = self.stat_table.row_update(idx, new_pokemon.stat_table)
+        current_hp = self.current_hp.at[idx].set(new_pokemon.current_hp)
+        new_obj = self.replace(
+            type_list=type, is_alive=is_alive, gender=gender, is_terastallized=is_terastallized, status=status,
+            stat_table=stat_table, current_hp=current_hp
         )
+        return new_obj
 
 
 
@@ -89,9 +93,6 @@ class Pokemon(DataclassArray):
         # add check for goggles
         return self.is_type(Type.GRASS)
 
-    @property
-    def current_hp(self):
-        return self.stat_table.current_hp
 
 
 

ninjax/side.py
@@ -81,11 +81,12 @@ class BattleState(DataclassArray):
 
     @property
     def active(self) -> Pokemon:
-        return self.team[self.active_index]
+        flattened_idx = (jnp.reshape(self.active_index, (2,)))
+        return self.team[[0,1], flattened_idx]
 
     @property
     def boosted_stats(self):
-        return self.active.stats * STAT_MULTIPLIER_LOOKUP[self.boosts.normal_boosts]
+        return jnp.floor(self.active.stats * STAT_MULTIPLIER_LOOKUP[6+self.boosts.normal_boosts])
 
     @property
     def accuracy_boosts(self):
@@ -93,7 +94,7 @@ class BattleState(DataclassArray):
 
     @property
     def current_hp(self):
-        return self.active.stat_table.current_hp
+        return self.active.current_hp
 
     def legal_switch_mask(self):
         # TODO: replace list comprehension with some kind of jax control flow
@@ -114,7 +115,7 @@ def add_boosts(state: BattleState, side_idx, idxs, vals) -> BattleState:
     return set_boosts(state, side_idx, StatBoosts(normal_boosts=new_boosts, acc_boosts=state.boosts.acc_boosts[side_idx]))
 
 def update_active(state: BattleState, side_idx, new_mon: Pokemon) -> BattleState:
-    new_team = state[side_idx].team.replace_row(state[side_idx].active_index, new_mon)
+    new_team = state.team.replace_row((side_idx, state[side_idx].active_index), new_mon)
     return state.replace(team=new_team)
 
 def clear_volatile_status(state: BattleState, side_idx) -> BattleState:
@@ -177,7 +178,7 @@ def swap_out(
     state = set_status(state, side_idx, status_ * no_status * is_poison_immune * is_not_flying)
 
     #check if switch in is still alive
-    active_hp = state[side_idx].active.stat_table.current_hp[side_idx]
+    active_hp = state[side_idx].active.current_hp[side_idx]
     is_alive = active_hp != 0
     active = state.active[side_idx].replace(is_alive=is_alive)
     state = update_active(state, side_idx, active)
@@ -200,10 +201,9 @@ def step_side_conditions(
     return key, state
 
 def take_damage_value(state: BattleState, defender_idx: int, damage: chex.Array) -> BattleState:
-    defending_side = state[defender_idx]
-    active = defending_side.active
+    active = state.active[defender_idx]
     # TODO: there are some effects that trigger based on damage taken, like mirror coat
-    new_health = jax.lax.clamp(0, (active.stat_table.current_hp-damage), active.max_hp)
+    new_health = jax.lax.clamp(0, (active.current_hp[defender_idx]-damage), active.max_hp)
     # check to make sure we don't accidentally revive a pokemon
     alive = jnp.logical_and(jnp.bool([new_health != 0]), active.is_alive)
     active = active.replace(current_hp=new_health, is_alive=alive)

ninjax/stats.py
@@ -38,7 +38,7 @@ class StatBoosts(DataclassArray):
         super().__post_init__()
         # use __setattr__ manually instead of attribute assignment since the class is frozen
         object.__setattr__(self, "normal_boosts", jnp.clip(self.normal_boosts, -6, 6))
-        object.__setattr__(self, "acc_boosts", jnp.clip(self.normal_boosts, -6, 6))
+        object.__setattr__(self, "acc_boosts", jnp.clip(self.acc_boosts, -6, 6))
 
     def replace_row(self, idx: int, new_boosts: 'StatBoosts'):
         normal_boosts = self.normal_boosts.at[idx].set(new_boosts.normal_boosts)
@@ -59,7 +59,6 @@ class StatTable(DataclassArray):
     evs: IntArray['*batch_size 6'] = field(default_factory=lambda: 84*jnp.ones(6, dtype=int32))
     # this seems to be the cleanest way to implement these since they need fancy initialization
     stats: IntArray['*batch_size 6'] = field(init=False, default=None)
-    current_hp: IntArray['*batch_size 1'] = field(init=False, default=None)
 
     def __post_init__(self) -> None:
         # make sure to call the super function, you fool, you absolute buffoon
@@ -69,16 +68,14 @@ class StatTable(DataclassArray):
         # self.stats = calculate_stats(self.level, self.nature, self.base_stats, self.ivs, self.evs))
         # self.current_hp = self.stats[0]
         object.__setattr__(self, 'stats', calculate_stats(self.level, self.nature, self.base_stats, self.ivs, self.evs))
-        object.__setattr__(self, 'current_hp', self.stats[...,0])
 
     def row_update(self, idx: int, new_stats: 'StatTable'):
         level = self.level.at[idx].set(new_stats.level)
         ivs = self.ivs.at[idx].set(new_stats.ivs)
         evs = self.evs.at[idx].set(new_stats.evs)
-        stats = self.stats.at[idx].set(new_stats.stats)
-        current_hp = self.current_hp.at[idx].set(new_stats.current_hp)
         nature = self.nature.row_update(idx, new_stats.nature)
-        return self.replace(level=level, nature=nature, ivs=ivs, evs=evs, stats=stats, current_hp=current_hp)
+        # so this is a problem because we calculate the current_hp in the post_init which runs even when we call replace
+        return self.replace(level=level, nature=nature, ivs=ivs, evs=evs)
 
 
 

ninjax/utils.py
@@ -5,8 +5,15 @@ from functools import partial
 import chex
 from flax import struct
 import jax
+from jaxlib.xla_extension import ArrayImpl
 import jax.numpy as jnp
 import numpy as np
+
+# i deserve to be tried at the hague for this line of code
+# dataclass_array checks if field have a hash as a proxy for being immutable
+# so i'm adding a dummy hash function to it here to avoid it throwing errors
+# I'm good at programming
+ArrayImpl.__hash__ = lambda : 0
 
 Array = jax.Array
 increase_mult = 1.1
@@ -39,11 +46,9 @@ TYPE_EFFECTIVENESS = jnp.array(
         [1, 1, 1/2, 1/2, 1/2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1/2, 2],
         [1, 1, 1/2, 1, 1, 1, 1, 2, 1/2, 1, 1, 1, 1, 1, 1, 2, 2, 1/2, 1]])
 
-@jax.jit
 def calculate_effectiveness_multiplier(attacking_type, defending_types) -> Array:
     return jnp.prod(TYPE_EFFECTIVENESS[attacking_type][defending_types])
 
-@jax.jit
 def calculate_stats(level: Array, nature: "Nature", base_stats: Array, ivs: Array, evs: Array):
     # initial part of compute
     stats: Array = jnp.floor_divide((2 * base_stats + jnp.floor_divide(evs, 4) + ivs) * level, 100) + 5 + jnp.array([1, 0, 0, 0, 0, 0]) * (level + 5)
@@ -56,7 +61,7 @@ def calculate_stats(level: Array, nature: "Nature", base_stats: Array, ivs: Arra
     return stats
 
 # computes damage before any multiplicative modifiers
-@jax.jit
+
 def base_damage_compute(
         attacker_level: int,
         attack_stat: int,
@@ -64,9 +69,6 @@ def base_damage_compute(
         base_power: int):
     return (2 * attacker_level / 5 + 2) * base_power * attack_stat / defence_stat / 50 + 2
 
-@partial(jax.jit, static_argnums=2)
-def static_len_array_access(array, index, length):
-    return jax.lax.switch(index, [lambda: array[i] for i in range(length)])
 
 
 